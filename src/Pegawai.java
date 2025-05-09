d8ddf11b927450b7e9b7970968b5941a77edf240
dfnc1 <dianfnc06@gmail.com>
2025-05-09 20:36:42 +0700

    Initial commit

--- src/Pegawai.java
+++ src/Pegawai.java
@@ -1,9 +1,5 @@
-public class Pegawai extends Sekolah {
-    protected String NIP, Jabatan ;
-
-    public Pegawai(String nama, String no_Tlp, String e_mail,  String NIP, String jabatan) {
-        super(null,null , nama, "", no_Tlp, e_mail, "");
-        this.NIP = NIP ;
-        Jabatan = jabatan ;
+public class Pegawai {
+    public static void main(String[] args) {
+        System.out.println("kon");
     }
 }
