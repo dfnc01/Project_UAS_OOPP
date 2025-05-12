0541f3a6db324cdbe43478f41e25928a398cc9d5
rmhyps1 <240605110123@student.uin-malang.ac.id>
2025-05-12 15:52:53 +0700

    edit

--- src/Keamanan/Satpam.java
+++ src/Keamanan/Satpam.java
@@ -1,6 +1,6 @@
 package Keamanan;
 
-public class Satpam extends Keamanan {
+public class Satpam extends  {
     public Satpam(String nama, String no_Tlp, String e_mail, String id_Petugas, String jabatan) {
         super(nama, no_Tlp, e_mail, id_Petugas, jabatan);
     }
